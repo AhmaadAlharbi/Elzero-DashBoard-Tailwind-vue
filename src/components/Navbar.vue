e2c022c6b030501426a43058ac2501bee5b79cd4
AhmadAlharbi <ahmaadzaid7@gmail.com>
2022-09-25 08:27:58 +0300

    started with navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav
+    class="flex flex-col items-star h-screen p-3 w-56 drop-shadow-2xl bg-white"
+  >
+    <h1 class="my-6">Elzero</h1>
+    <div class="text-left space-y-3 pl-3">
+      <h3 class="bg-gray-100 p-2 rounded-md">Dashboard</h3>
+      <h3>Settings</h3>
+      <h3>Profile</h3>
+      <h3>Projects</h3>
+      <h3>Courses</h3>
+      <h3>Friends</h3>
+      <h3>Files</h3>
+      <h3>Plans</h3>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
